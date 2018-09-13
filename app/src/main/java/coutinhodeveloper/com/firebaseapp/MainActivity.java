e18562c1e7c7d98ecdf6269101e7a3c5355ae688
Guilherme Coutinho <guilhermecoutinho7777@gmail.com>
2018-09-13 20:06:19 -0300

    firebase configurado

--- app/src/main/java/coutinhodeveloper/com/firebaseapp/MainActivity.java
+++ app/src/main/java/coutinhodeveloper/com/firebaseapp/MainActivity.java
@@ -3,11 +3,19 @@ package coutinhodeveloper.com.firebaseapp;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends Activity {
+
+    private DatabaseReference firebaseReferencia = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseReferencia.child("pontos").setValue("100");
+        
     }
 }
